19ceb231566933f9c30e927b4f1eac660c3d2d95
Raj <raajsinha7797@gmail.com>
2020-08-25 18:11:41 +0530

    Create credentials.py

---
+++ Twitter Bot/credentials.py
@@ -0,0 +1,8 @@
+# This is just a sample file. You need to 
+# edit this file. You need to get these 
+# details from your Twitter app settings. 
+
+consumer_key = 'TZOCY856j0YL8XPlyzK325UOT' 
+consumer_secret = 'zD7be4s8eFdNfAKnIMG1K3j5pgXRhcDqvZdPbSW3urEpIA0lE9' 
+access_token = '1180841409676550144-iSeJKD8RE90cZWtzXxTa6RCtMhZben' 
+access_token_secret = 'Zrbcgis07qwQZLSWg4tscawrUnsVWY5IOsV5ld3y48Gsd'
